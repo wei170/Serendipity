f96608ea5d97fb6c604a9b53e290aafa82a0f3ce
wei170 <guocheng@guochengwei.com>
2016-09-18 07:02:38 -0400

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Guocheng's stuff
+  # Disable Rails's static asset server (Apache or nginx will already do this)  
+  config.serve_static_assets = false
+  
+  # Compress JavaScripts and CSS and Fonts
+  config.assets.compress = true
+  
+  # Don't fallback to assets pipeline if a precompiled asset is missed  
+  config.assets.compile = false
+  
+  # Generate digests for assets URLs  
+  config.assets.digest = true
 end
